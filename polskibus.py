b306f8875325312a85f1da8f893655fd2c6edc26
Rafal Czaja <mojepiwo@gmail.com>
2014-06-14 00:04:43 +0200

    Excluding specified cities from destination list.

--- polskibus.py
+++ polskibus.py
@@ -3,7 +3,7 @@
 """Polskibus.
 Usage:
 	polskibus.py list [<city>]
-	polskibus.py check <source> <date> [--promo=<promo> --max-days=<max-days> ((-)<city>)...]
+	polskibus.py check <city> <date> [--pax=PAX --promo=PROMO --max-days=MAX (-e CITY)...]
 """
 import sys
 import requests
@@ -32,23 +32,27 @@ class Polskibus:
 		for city in soup.findAll('option')[1:]:
 			self.destinations[city.text] = city['value']
 
-		# return o
 
 if __name__ == '__main__':
 	args = docopt(__doc__, version='Polskibus cos tam 0.1')
 
+	polskibus = Polskibus()
+	polskibus.get_origins()
 	if args['list']:
-		polskibus = Polskibus()
-		polskibus.get_origins()
 		if not args['<city>']:
 			print "Available cities:"
 			for city in polskibus.origins:
 				print city
 		else:
-			print "Destination cities available for {}".format(args['<city>'][0])
+			print "Destination cities available for {}:".format(args['<city>'])
 			# for city in polskibus.get_destinations(args['<city>'][0]):
-			polskibus.get_destinations(args['<city>'][0].decode('utf-8'))
+			polskibus.get_destinations(args['<city>'].decode('utf-8'))
 			for city in polskibus.destinations:
 				print city
 	elif args['check']:
-		pass
+		polskibus.get_destinations(args['<city>'].decode('utf-8'))		
+		for e in args['CITY']:
+			try:
+				del polskibus.destinations[e]
+			except KeyError:
+				pass
